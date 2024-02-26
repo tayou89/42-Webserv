3b128bdd18aab28e2c396bdb53de36495c17f87c
littlepark2 <physics.jinyk@gmail.com>
2024-02-26 12:01:03 +0900

    added | cp constructor, assignment operator to protocol | Protocol.cpp, Protocol.hpp

--- include/Protocol.hpp
+++ include/Protocol.hpp
@@ -6,48 +6,47 @@
 #include "utils.hpp"
 
 class Protocol {
-    public :
-		Protocol();
-        ~Protocol();
+public:
+  Protocol();
+  ~Protocol();
+  Protocol(const Protocol &ref);
+  Protocol &operator=(const Protocol &ref);
 
-		void	readStartLine(std::string startLine, int large_client_header_buffers);
-		void	readHeader(std::string header);
-		void	checkValidHeader(std::string key, std::string value);
-		void	checkContentLength(int client_max_body_size);
-		void	readBody(std::string body);
-		std::string		create200Response();
-		std::string		create204Response();
-		std::string		create400Response();
-		std::string		create404Response();
-		std::string		create405Response();
-		std::string 	create413Response();
-		std::string		create414Response();
-		std::string		create500Response();
-		std::string 	create505Response();
+  void readStartLine(std::string startLine, int large_client_header_buffers);
+  void readHeader(std::string header);
+  void checkValidHeader(std::string key, std::string value);
+  void checkContentLength(int client_max_body_size);
+  void readBody(std::string body);
+  std::string create200Response();
+  std::string create204Response();
+  std::string create400Response();
+  std::string create404Response();
+  std::string create405Response();
+  std::string create413Response();
+  std::string create414Response();
+  std::string create500Response();
+  std::string create505Response();
 
-		std::string getRequestMethod() const;
-		std::string getRequestURI() const;
-		std::string getRequestBody() const;
-		std::map<std::string, std::string> getRequestHeader() const;
-		std::string getRequestHeader(std::string key) const;
-		void		setRequestHeader(std::string key, std::string value);
+  std::string getRequestMethod() const;
+  std::string getRequestURI() const;
+  std::string getRequestBody() const;
+  std::map<std::string, std::string> getRequestHeader() const;
+  std::string getRequestHeader(std::string key) const;
+  void setRequestHeader(std::string key, std::string value);
 
-		void		setResponseHeader(std::string key, std::string value);
-		void		setResponseBody(std::string body);
-		void		setResponse(std::string response);
-		std::string	getResponse();
+  void setResponseHeader(std::string key, std::string value);
+  void setResponseBody(std::string body);
+  void setResponse(std::string response);
+  std::string getResponse();
 
-    private :
-        Protocol(const Protocol&);
-        Protocol& operator=(const Protocol&);
-		std::string _request;
-        std::map<std::string, std::string> _requestHeader;
-		std::string _requestBody;
-		std::string _requestMethod;
-		std::string _requestURI;
+private:
+  std::string _request;
+  std::map<std::string, std::string> _requestHeader;
+  std::string _requestBody;
+  std::string _requestMethod;
+  std::string _requestURI;
 
-		std::map<std::string, std::string> _responseHeader;
-		std::string _responseBody;
-		std::string _response;
-    
+  std::map<std::string, std::string> _responseHeader;
+  std::string _responseBody;
+  std::string _response;
 };
