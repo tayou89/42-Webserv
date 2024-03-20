fa6c6f41b617441b9a0c4d53d971e25ee74800fb
yedididi <iyeji366@google.com>
2024-03-20 11:33:46 +0900

    feature | color cookie ok | cookie.cpp

--- protocol/Cookie.cpp
+++ protocol/Cookie.cpp
@@ -84,7 +84,7 @@ void Cookie::controlCookies(std::map<std::string, std::string> header, std::stri
     if (_queryStringExistance == 1) //쿼리스트링이 존재함
     {
         _resCookieHeaderString.clear();
-        _resCookieHeaderString += "color=" + _queryString["color"] + "; ";
+        _resCookieHeaderString = "color=" + _queryString["color"] + "; ";
         // _resCookieHeaderString += "size=" + _queryString["size"] + "; ";
         // _resCookieHeaderString += "Expires=" + convertIntoRealTime(getCookieTime(0, 1, 0)) + ";";
         makeBody(_queryString["color"], _queryString["size"]);
@@ -92,15 +92,19 @@ void Cookie::controlCookies(std::map<std::string, std::string> header, std::stri
     }
     else if (itrHeader == header.end()) //쿼리스트링과 쿠키 다 없음
     {
-        makeBody("000000", "15");
         _resCookieHeaderString.clear();
+        _resCookieHeaderString = "color=000000; ";
+        makeBody("000000", "15");
         std::cout << "query string and cookie does not exist, color is:" << "000000" << ", and size is:" << "15" << std::endl;
     }
     else //쿼리스트링은 없지만 쿠키헤더값은 있는 경우
     {
-        setCookieHeader(itrHeader->second);
-        makeBody(_reqCookieHeader["color"], _reqCookieHeader["size"]);
         _resCookieHeaderString.clear();
+        setCookieHeader(itrHeader->second);
+        if (_reqCookieHeader.find("size") == _reqCookieHeader.end())
+            _reqCookieHeader.insert(std::make_pair("size", "15"));
+        _resCookieHeaderString = "color=" + _reqCookieHeader["color"] + "; ";
+        makeBody(_reqCookieHeader["color"], _reqCookieHeader["size"]);
         std::cout << "query string does not, but cookie header exists, color is:" << _reqCookieHeader["color"] << ", and size is:" << _reqCookieHeader["size"] << std::endl;
     }
 }
@@ -117,7 +121,7 @@ void    Cookie::setCookieHeader(std::string rawCookie)
         tmp = rawCookie.substr(pos, index - pos);
         std::string key = splitBefore(tmp, "=");
         std::string value = splitAfter(tmp, "=");
-        this->_reqCookieHeader.insert(std::make_pair(key, value));
+        this->_reqCookieHeader[key] = value;
         if (index == std::string::npos)
             break;
         pos = index + 2;
