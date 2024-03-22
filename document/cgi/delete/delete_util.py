4eb00a21f2c25d5ac372223fc428e024fee4d4b5
tayou89 <ysh00718@gmail.com>
2024-03-22 16:52:10 +0900

    fix delete.css

--- document/cgi/delete/delete_util.py
+++ document/cgi/delete/delete_util.py
@@ -1,5 +1,5 @@
 def	getDeleteStringList(fileList):
 	buttonStringList = ''
 	for fileName in fileList:
-		buttonStringList +=  "<li><span><a href='/show/file.py?file={}'>{}</a></span><button onclick=\"deleteFile('{}')\">delete</button></li>".format(fileName, fileName, fileName)
+		buttonStringList +=  "<li><span id='filename'><a href='/show/file.py?file={}'>{}</a></span><span id='button'><button onclick=\"deleteFile('{}')\">delete</button></span></li>".format(fileName, fileName, fileName)
 	return (buttonStringList)
